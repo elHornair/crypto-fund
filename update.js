49b141b4b3509f700590bf0a015024f876e84482
Alain Horner <alain.hornair@gmail.com>
2020-08-12 20:50:59 +0200

    get basic market data and write it to file

---
+++ update.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+const axios = require('axios');
+const apiBaseUrl = 'https://api.coingecko.com/api/v3/'
+
+async function getBasicData() {
+	try {
+		const response = await axios.get(`${apiBaseUrl}coins/markets`, {
+			params: {
+				vs_currency: 'usd',
+				order: 'market_cap_desc',
+				per_page: 15,
+				page: 1,
+				sparkline: false
+			}
+		});
+
+		return response.data.map((coin) => {
+			return {
+				id: coin.id,
+				symbol: coin.symbol,
+				name: coin.name,
+				current_price: coin.current_price,
+				market_cap: coin.market_cap,
+				market_cap_rank: coin.market_cap_rank
+			}
+		})
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+async function getMarketData() {
+	let marketData = await getBasicData();
+
+	fs.writeFile('data.json', JSON.stringify(marketData), (err) => {
+		if (err) {
+			return console.log('Error writing file: ', err);
+		}
+
+		console.log('Current market data written to data.json');
+	});
+}
+
+getMarketData();
