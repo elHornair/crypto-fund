9686ab3a34e5485dab246afd9461f5b94510f439
Alain Horner <alain.hornair@gmail.com>
2020-08-23 19:49:08 +0200

    basic coins component

---
+++ src/components/Coins.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="coins">
+    <h1>Coins</h1>
+    <p>{{ title }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Coins',
+  props: {
+    title: String
+  }
+}
+</script>
